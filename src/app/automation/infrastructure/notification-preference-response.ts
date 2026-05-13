e4d0a3cf7db43ceda93b6990cb731ca846d09e97
ECEB0704 <gogli.chavez@gmail.com>
2026-05-13 18:33:30 -0500

    feat(automation): add NotificationPreferencesApi, NotificationPreferencesApiEndpoint, NotificationPreferenceAssembler, and response models for managing notification preferences

---
+++ src/app/automation/infrastructure/notification-preference-response.ts
@@ -0,0 +1,13 @@
+﻿import { BaseResource, BaseResponse } from '../../shared/infrastructure/base-response';
+
+export interface NotificationPreferenceResource extends BaseResource {
+  id: number;
+  channel: string;
+  isEnabled: boolean;
+  threshold: string;
+  description: string;
+}
+
+export interface NotificationPreferencesResponse extends BaseResponse {
+  notificationPreferences: NotificationPreferenceResource[];
+}
